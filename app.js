616c542791b415ecf27a82e10951d8603c6fda95
MCA-Reports <info@bmcag.org>
2026-07-20 11:39:19 +0600

    initial

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let favoriteChannelIds = [];
 let currentlyPlayingId = null;
 const FAVORITES_KEY = 'iptvFavorites';
 const THEME_KEY = 'iptvTheme';
-const M3U_STREAMS_URL = 'https://iptv-org.github.io/iptv/streams.m3u';
+const M3U_STREAMS_URL = 'https://iptv-org.github.io/iptv/index.m3u';
 
 const countryFilter = document.getElementById('countryFilter');
 const sportFilter = document.getElementById('sportFilter');
